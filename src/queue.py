6e0f5c5b1361c780b581b6223ee201f7a9e36d8a
Julia Tsyganok <julia.tsyganok.9@yandex.ru>
2025-11-15 20:25:43 +0300

    Добавлены классы стэка и очереди

---
+++ src/queue.py
@@ -0,0 +1,40 @@
+class Queue:
+    def __init__(self):
+        """
+        Создание списка для хранения
+        """
+        self.items = []
+    
+    def enqueue(self, x: int) -> None:
+        """
+        Добавление элемента
+        """
+        self.items.append(x)
+    
+    def dequeue(self) -> int:
+        """
+        Удаляет элемента. Метод возвращает удалённый элемент
+        """
+        if self.is_empty():
+            raise Exception("Пустая очередь")  
+        return self.items.pop(0)
+    
+    def front(self) -> int:
+        """
+        Получение первого элемента
+        """
+        if self.is_empty():
+            raise Exception("Пустая очередь") 
+        return self.items[0] 
+    
+    def is_empty(self) -> bool:
+        """
+        Проверка исключения
+        """
+        return len(self.items) == 0
+    
+    def __len__(self) -> int:
+        """
+        Получение длины стэка
+        """
+        return len(self.items)
---
+++ src/stack.py
@@ -0,0 +1,51 @@
+class Stack:
+    """
+    Stack на list
+    """
+    def __init__(self):
+        """
+        Создание списка для хранения
+        """
+        self.items = [] 
+    
+    def push(self, x: int) -> None:
+        """
+        Добавляет элемента
+        """
+        self.items.append(x)
+    
+    def pop(self) -> int:
+        """
+        Удаление элемента из стэка. Метод возвращает удалённый элемент
+        """
+        if self.is_empty():
+            raise Exception("Пустой стэк")
+        return self.items.pop()
+    
+    def peek(self) -> int:
+        """
+        Получение последнего элемента
+        """
+        if self.is_empty():
+            raise Exception("Пустой стэк")
+        return self.items[-1]  
+    
+    def is_empty(self) -> bool:
+        """
+        Проверка исключения
+        """
+        return len(self.items) == 0
+    
+    def __len__(self) -> int:
+        """
+        Получение длины стэка
+        """
+        return len(self.items)
+    
+    def min(self) -> int:
+        """
+        Получение минимального элемента в стэке
+        """
+        if self.is_empty():
+            raise Exception("Пустой стэк")
+        return min(self.items)
