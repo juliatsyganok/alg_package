29a2697ab85e88e3591cf89c20234c0004cef164
juliatsyganok <julia.tsyganok.9@yandex.ru>
2025-11-23 14:27:18 +0300

    Изменён класс стэка для посика минимума в кортеже

--- src/stack.py
+++ src/stack.py
@@ -1,18 +1,22 @@
 class Stack:
     """
-    Stack на list
+    Stack на list 
     """
     def __init__(self):
         """
         Создание списка для хранения
         """
-        self.items = [] 
+        self.items = []
     
     def push(self, x: int) -> None:
         """
-        Добавляет элемента
+        Добавляет элемент в виде кортежа
         """
-        self.items.append(x)
+        if self.is_empty():
+            self.items.append((x, x))
+        else:
+            m = min(x, self.items[-1][1])
+            self.items.append((x, m))
     
     def pop(self) -> int:
         """
@@ -20,32 +24,32 @@ class Stack:
         """
         if self.is_empty():
             raise Exception("Пустой стэк")
-        return self.items.pop()
-    
+        return self.items.pop()[0]
+
     def peek(self) -> int:
         """
         Получение последнего элемента
         """
         if self.is_empty():
             raise Exception("Пустой стэк")
-        return self.items[-1]  
-    
+        return self.items[-1][0]
+
     def is_empty(self) -> bool:
         """
         Проверка исключения
         """
         return len(self.items) == 0
-    
+
     def __len__(self) -> int:
         """
         Получение длины стэка
         """
         return len(self.items)
-    
+
     def min(self) -> int:
         """
         Получение минимального элемента в стэке
         """
         if self.is_empty():
             raise Exception("Пустой стэк")
-        return min(self.items)
+        return self.items[-1][1] 
